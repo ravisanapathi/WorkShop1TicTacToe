8091e0129a6374615156fd2e87c37c74fc909cc7
ravisanapathi <ravisanapathi007@gmail.com>
2022-08-18 10:54:48 +0530

    Added UC-2 Select Letter

--- com/bridgelabz/PrintBoard.java
+++ com/bridgelabz/PrintBoard.java
@@ -1,18 +1,32 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
 public class PrintBoard {
 	static char[] board = new char[10];
+	static char playerLetter;
+	static char computerLetter;
 	public static void main(String[] args)
 	{
 		createEmptyBoard();
+		selectLetter();
 
 	}
 
+	//UC1
 	private static void createEmptyBoard()
 	{
 		for(int index=1; index<board.length;index++)
 		{
 			board[index] = ' ';
 		}
+	}
+
+	//UC2
+	private static void selectLetter()
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Choose a letter :: X or O : ");
+		playerLetter = scanner.next().toUpperCase().charAt(0);
+		computerLetter = (playerLetter == 'X') ? 'O' : 'X';
 	}
 }
